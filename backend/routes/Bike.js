506a89657b5a0ec741ef87c725c47a2b4e018570
israel itach <israely6655@gmail.com>
2023-05-29 15:43:00 +0300

    order bike

--- backend/routes/Bike.js
+++ backend/routes/Bike.js
@@ -29,19 +29,42 @@ router.put("/:id", (req, res) => {
   const { id } = req.params;
   const { isAvailable } = req.body;
 
-  pool.query(
-    `UPDATE bike SET isAvailable = ? WHERE id = ?`,
-    [isAvailable ? 1 : 0, id],
-    (error, results) => {
-      if (error) {
-        console.error(error);
-        res.status(500).json({ error: "Internal server error" });
-      } else {
-        console.log(results);
-        res.status(200).end();
-      }
+  pool.query(`UPDATE bike SET isAvailable = ? WHERE id = ?`, [isAvailable ? 1 : 0, id], (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
+    } else {
+      console.log(results);
+      res.status(200).end();
     }
-  );
+  });
+});
+
+// Order Bike
+router.put("/order/:id", (req, res) => {
+  // מזהה אופנים
+  const { id } = req.params;
+
+  // לקבל את האופניים ע"פ מזהה האופנים
+  const sql = `SELECT * FROM bike WHERE id = ?`;
+  pool.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: "Internal server error" });
+    // אם זמין, להזמין
+    else if (results.length && results[0].isAvailable) {
+      pool.query(`UPDATE bike SET isAvailable = 0 WHERE id = ?`, [id], (error, results) => {
+        if (error) {
+          console.error(error);
+          res.status(500).json({ error: "Internal server error" });
+        } else {
+          console.log(results);
+          res.status(204).end();
+        }
+      });
+      // אם לא זמין, להחזיר הודעת שגיאה
+    } else {
+      res.status(400).json({ error: "האופניים לא זמינות" });
+    }
+  });
 });
 
 // Add a new bike
@@ -50,7 +73,7 @@ router.post("/", (req, res) => {
   const sql = `INSERT INTO bike ( name, description, image_url) VALUES ('${name}', '${description}', '${image_url}')`;
   pool.query(sql, (err, results) => {
     if (err) console.log(err);
-    else { 
+    else {
       // send the new bike from db
       const sql = `SELECT * FROM bike WHERE id = ${results.insertId}`;
       pool.query(sql, (err, results) => {
